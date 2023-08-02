aa95f0ecf9d729819651cfd185c69d13955d5e2c
FARIDA <140826748+begimbaikyzyfarida@users.noreply.github.com>
2023-08-02 14:20:30 +0600

    Create MyClass.java

---
+++ MyClass.java
@@ -0,0 +1,15 @@
+public class MyClass {
+    String name;
+    String surname;
+    int age;
+    String[] favouritelesson;
+    String dishes;
+
+    public  MyClass(String name, String surname, int age, String[] favouritelesson, String dishes) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.favouritelesson = favouritelesson;
+        this.dishes= dishes;
+    }
+}
